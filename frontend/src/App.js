d2bf58850f9276bc10e42cf26fe432e0b1dfad73
Mostafa Osama <31553087+Battarshuck@users.noreply.github.com>
2023-08-30 13:19:28 +0300

    added logo component, and added the signup page

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -2,6 +2,7 @@ import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
 
 import './App.css';
 import Login from './components/login/login'
+import Signup from './components/signup/signup'
 
 function App() {
   return (
@@ -9,6 +10,7 @@ function App() {
       <Routes>
         <Route exact path="/" element={<Login/>} />
         <Route path="/login" element={<Login/>} />
+        <Route path="/signup" element={<Signup/>} />
         {/* <Route path="*" element={<NotFound/>} /> */}
       </Routes>
     </Router>
--- frontend/src/components/login/login.js
+++ frontend/src/components/login/login.js
@@ -1,6 +1,7 @@
 import React, { useState } from 'react';
 import { useNavigate,Link } from 'react-router-dom';
 import axios from 'axios';
+import Logo from '../logo/logo';
 
 const Login = () => {
   const navigate = useNavigate();
@@ -26,9 +27,7 @@ const Login = () => {
   return (
     <section className="bg-gray-50 dark:bg-gray-900">
       <div className="flex flex-col items-center justify-center px-6 py-8 mx-auto md:h-screen lg:py-0">
-          <a href="https://bitnine.net/" className="flex items-center mb-6 text-2xl font-semibold text-gray-900 dark:text-white">
-              <img className="w-27 h-20 mr-2" src="https://bitnine.net/wp-content/uploads/2021/02/b_logo.png" alt="logo"></img>
-          </a>
+          {<Logo/>}
           <div className="w-full bg-white rounded-lg shadow dark:border md:mt-0 sm:max-w-md xl:p-0 dark:bg-gray-800 dark:border-gray-700">
               <div className="p-6 space-y-4 md:space-y-6 sm:p-8">
                   <h1 className="text-xl font-bold leading-tight tracking-tight text-gray-900 md:text-2xl dark:text-white">
