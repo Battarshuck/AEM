90e006e0d55a77642f6397939e9fb196e0f2ad0c
Emmanuel Allison <emmallison13@gmail.com>
2023-09-03 14:45:32 +0100

    feat: username added for signing up

--- backend/src/components/account/v1/validation/index.js
+++ backend/src/components/account/v1/validation/index.js
@@ -4,6 +4,8 @@ const Joi = require("joi");
  * Validation schema for the sign up object.
  */
 const signUpSchema = Joi.object({
+	username: Joi.string().required().pattern(new RegExp("^[a-zA-Z0-9]{3,30}$")),
+
 	email: Joi.string().required().email(),
 
 	password: Joi.string().required().min(8).max(64),
