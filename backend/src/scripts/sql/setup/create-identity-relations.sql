849583e2d87bd2d92b87c165992d5396422262d6
Emmanuel Allison <emmallison13@gmail.com>
2023-09-03 22:15:47 +0100

    fix: added UNIQUE constraint on columns

--- backend/src/scripts/sql/setup/create-identity-relations.sql
+++ backend/src/scripts/sql/setup/create-identity-relations.sql
@@ -4,10 +4,10 @@ CREATE SCHEMA IF NOT EXISTS identity AUTHORIZATION aem;
 -- User table for AEM.
 CREATE TABLE IF NOT EXISTS identity.user (
   id                  BIGINT GENERATED ALWAYS AS IDENTITY PRIMARY KEY,
-  email               VARCHAR(255) NOT NULL,
-  normalised_email    VARCHAR(255) NOT NULL,
-  username            VARCHAR(100) NOT NULL,
-  normalised_username VARCHAR(100) NOT NULL,
+  email               VARCHAR(255) NOT NULL UNIQUE,
+  normalised_email    VARCHAR(255) NOT NULL UNIQUE,
+  username            VARCHAR(100) NOT NULL UNIQUE,
+  normalised_username VARCHAR(100) NOT NULL UNIQUE,
   password            TEXT NOT NULL,
   created_date        TIMESTAMP WITH TIME ZONE NOT NULL DEFAULT current_timestamp
 );
